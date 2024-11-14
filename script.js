c07b9957e86a6a13ae831bd8cccf829128f78eb4
himanshu <r99.himanshu@gmail.com>
2024-11-14 12:41:40 +0530

    feat: add snake head movement feature to game logic

--- script.js
+++ script.js
@@ -1,7 +1,42 @@
-import { addElements } from './utils.js';
+import { addElements, move } from './utils.js';
 
 // Get the Game Board element from the DOM
 const gameBoard = document.getElementById('game-board');
 
 // Create 16x16 Game Board Grid by adding 256 'div' elements with the class 'cell'
 addElements(gameBoard, 'div', 256, 'cell');
+
+// Get all cell elements
+const cells = document.getElementsByClassName('cell');
+
+// Set initial position of Snake head
+let snakeHeadIndex = 119;
+// Set Snake Head color
+cells[snakeHeadIndex].style.background = '#00CE76';
+
+// Set initial direction of Snake movement
+let direction = 'top';
+
+// Listen for arrow key presses to change Snake direction
+document.addEventListener("keydown", (event) => {
+    switch (event.key) {
+        case "ArrowUp":
+            direction = 'top';
+            break;
+        case "ArrowRight":
+            direction = 'right';
+            break;
+        case "ArrowDown":
+            direction = 'bottom';
+            break;
+        case "ArrowLeft":
+            direction = 'left';
+            break;
+    }
+});
+
+// Start game loop - moves Snake every 0.25s
+setInterval(() => {
+    // Update Snake Head position
+    snakeHeadIndex = move(cells, snakeHeadIndex, direction);
+}, 250);
--- utils.js
+++ utils.js
@@ -5,7 +5,33 @@ export const addElements = (parentElement, elementName, count, className) => {
         const childElement = document.createElement(elementName);
         childElement.classList.add(className);
 
-        // Add each element to a parent element
+        // Add each element to parent element
         parentElement.appendChild(childElement);
     }
+}
+
+// Function to move snake in specified direction
+export const move = (cells, index, direction) => {
+    // Clear current position
+    cells[index].style.background = `#292929`;
+
+    // Update snake head position based on direction
+    if (direction === 'top') {
+        index = index - 16;
+    }
+    else if (direction === 'right') {
+        index = index + 1;
+    }
+    else if (direction === 'bottom') {
+        index = index + 16;
+    }
+    else if (direction === 'left') {
+        index = index - 1;
+    }
+
+    // Color new position
+    cells[index].style.background = '#00CE76';
+
+    // Return the new snake head index
+    return index;
 }
